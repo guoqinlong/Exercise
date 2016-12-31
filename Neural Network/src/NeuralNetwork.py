518180c0e785af48ae0e2a3c8b281c4a04564783
郭秦龙 <guoqinlong@gmail.com>
2016-12-31 21:26:43 +0800

    modify show data to plot decision boundary

--- Neural Network/src/NeuralNetwork.py
+++ Neural Network/src/NeuralNetwork.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sklearn.datasets
+import sklearn.linear_model
 import matplotlib.pyplot as plt
 
 
@@ -12,15 +13,30 @@ def random_data():
     return sklearn.datasets.make_moons(200, noise=0.20)
 
 
-def show_data(x, y):
-    """
-    :param x:
-    :param y:
-    """
-    plt.scatter(x[:, 0], x[:, 1], s=40, c=y)
+def plot_decision_boundary(pred_func, x, y):
+
+    # Set min and max values and give it some padding
+    x_min, x_max = x[:, 0].min() - .5, x[:, 0].max() + .5
+    y_min, y_max = x[:, 1].min() - .5, x[:, 1].max() + .5
+    h = 0.01
+
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+
+    # Predict the function value for the whole gid
+    z = pred_func(np.c_[xx.ravel(), yy.ravel()])
+    z = z.reshape(xx.shape)
+
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, z)
+    plt.scatter(X[:, 0], x[:, 1], c=y)
     plt.show()
 
 
 if __name__ == "__main__":
     X, Y = random_data()
-    show_data(X, Y)
+
+    clf = sklearn.linear_model.LogisticRegressionCV()
+    clf.fit(X, Y)
+
+    plot_decision_boundary(lambda x: clf.predict(x), X, Y)
