eb537010da31c5e5964ce40aab0a5d773dc934a8
guoqinlong <guoqinlong@gmail.com>
2017-01-02 20:45:18 +0800

    add the forward propagation

--- Neural Network/src/RecurrentNeuralNetwork.py
+++ Neural Network/src/RecurrentNeuralNetwork.py
@@ -4,6 +4,11 @@ import itertools
 import nltk
 import numpy as np
 
+
+def softmax():
+    pass
+
+
 class RNNNumpy:
 
     def __init__(self, word_dim, hidden_dim, bptt_truncate=4):
@@ -15,6 +20,25 @@ class RNNNumpy:
         self.U = np.random.uniform(- np.sqrt(1./word_dim), np.sqrt(1./word_dim), (hidden_dim, word_dim))
         self.V = np.random.uniform(- np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (word_dim, hidden_dim))
         self.W = np.random.uniform(- np.sqrt(1./hidden_dim), np.sqrt(1./hidden_dim), (hidden_dim, hidden_dim))
+
+    def forward_propagation(self, x):
+        # The total number of time steps
+        T = len(x)
+        # During forward propagation, we save all hidden states in s because we need them later.
+        # We add one additional element for the initial hidden layer, which we set to 0
+        s = np.zeros((T + 1, self.hidden_dim))
+        s[-1] = np.zeros(self.hidden_dim)
+
+        #The output at each time step. Again, we save them for later.
+        o = np.zeros((T, self.hidden_dim))
+
+        #For each time step
+        for t in np.arange(T):
+            # Note that we are indexing U by x[t], This is the same as multiplying U with a one-hot vector.
+            s[t] = np.tanh(self.U[:, x[t]] + self.W.dot(s[t - 1]))
+            o[t] = softmax(self.V.dot(s[t]))
+
+        return [o, s]
 
 if __name__ == '__main__':
     vocabulary_size = 8000
