1aec0c37ba72cfac2616daf536df684fc935fd24
guoqinlong <guoqinlong@gmail.com>
2017-01-16 23:16:25 +0800

    add the head of cnn.

---
+++ Neural Network/src/ConvolutionNeuralNetwork.py
@@ -0,0 +1,7 @@
+class TextCNN(object):
+    """
+    A CNN for text classification.
+    Used an embedding layer, followed by a convolutional, max-pooling and softmax layer.
+    """
+    def __init__(self, sequence_length, num_classes, vocab_size, embedding_size, filter_sizes, num_filters):
+        pass
