ef544796cafe2699450948c005c5880f434b9802
guoqinlong <guoqinlong@gmail.com>
2017-01-19 19:24:14 +0800

    add the convolution and max_pool layer

--- Neural Network/src/ConvolutionNeuralNetwork.py
+++ Neural Network/src/ConvolutionNeuralNetwork.py
@@ -16,12 +16,40 @@ class TextCNN(object):
             self.embedded_chars = tf.nn.embedding_lookup(W, self.input_x)
             self.embedded_chars_expand = tf.expand_dims(self.embedded_chars, -1)
 
+        pooled_outputs = []
+        for i, filter_size in enumerate(filter_sizes):
+            # Convolution layer
+            filter_shape = [filter_size, embedding_size, 1, num_filters]
+            W = tf.variables(tf.truncated_normal(filter_shape, stddev=0.1), name='W')
+            b = tf.variables(tf.constant(0.1, shape=[num_filters]), name='b')
+            conv = tf.conv2d(
+                input=self.embedded_chars_expand,
+                filter=W,
+                strides=[1, 1, 1, 1],
+                padding='VALID',
+                name='conv'
+            )
+            # Apply nonlinearity
+            h = tf.nn.relu(tf.nn.bias_add(conv, b), name='h')
+            # Max-pooling over the output
+            pooled = tf.nn.max_pool(
+                    value=h,
+                    ksize=[1, sequence_length - filter_size + 1, 1, 1],
+                    strides=[1, 1, 1, 1],
+                    padding='VALID',
+                    name='pooled'
+            )
+            pooled_outputs.append(pooled)
+        num_filters_total = num_filters * len(filter_sizes)
+        self.h_pool = tf.concat(3, pooled_outputs)
+        self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
+
 
 if __name__ == '__main__':
     a = tf.placeholder(tf.int16)
     b = tf.placeholder(tf.int16)
-    add = tf.add(a, b)
-    mul = tf.mul(a, b)
-    with tf.Session() as sess:
+    with tf.device('/cpu:0'), tf.Session() as sess:
+        add = tf.add(a, b)
+        mul = tf.mul(a, b)
         print(sess.run(add, feed_dict={a: 3, b: 2}))
         print(sess.run(mul, feed_dict={a: 4, b: 3}))
