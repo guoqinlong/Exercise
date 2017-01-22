4a94a4fbb9f9d338e7cd7ab56a093b716b45be97
guoqinlong <guoqinlong@gmail.com>
2017-01-22 19:47:09 +0800

    add the batch

--- Neural Network/src/data_helpers.py
+++ Neural Network/src/data_helpers.py
@@ -36,8 +36,28 @@ def load_data_and_labels(positive_data_file, negative_data_file):
     # Split by words
     x_text = positive_examples + negative_examples
     x_text = [clean_str(sent) for sent in x_text]
-    #Generate labels
+    # Generate labels
     positive_labels = [[0, 1] for _ in positive_examples]
     negative_labels = [[1, 0] for _ in negative_examples]
-    y = np.concatenate(positive_examples, negative_examples, 0)
+    y = np.concatenate([positive_labels, negative_labels], 0)
     return [x_text, y]
+
+
+def batch_iter(data, batch_size, num_epochs, shuffle=True):
+    """
+    Generates a batch iterator for a dataset.
+    """
+    data = np.array(data)
+    data_size = len(data)
+    num_batches__per_epoch = int((data_size - 1) / batch_size) + 1
+    for epoch in range(num_epochs):
+        # Shuffle the data in each epoch
+        if shuffle:
+            shuffle_indices = np.random.permutation(np.arange(data_size))
+            shuffled_data = data[shuffle_indices]
+        else:
+            shuffled_data = data
+        for batch_num in range(num_batches__per_epoch):
+            start_index = batch_num * batch_size
+            end_index = min((batch_num + 1) * batch_size, data_size)
+            yield shuffled_data[start_index: end_index]
