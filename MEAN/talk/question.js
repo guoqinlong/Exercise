bea465ddbf41e19a7cb907123c4e2b898a8a80d6
guoqinlong <guoqinlong@gmail.com>
2017-01-25 19:35:53 +0800

    add MEAN basic

---
+++ MEAN/talk/question.js
@@ -0,0 +1,9 @@
+/**
+ * Created by T440P on 2017/1/24.
+ */
+var answer = "Noe that's a good question";
+
+module.exports.ask = function(question) {
+    console.log(question);
+    return answer;
+};
