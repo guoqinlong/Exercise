e28037c2ba289b438bded13d3ec7a40f47a1dc56
guoqinlong <guoqinlong@gmail.com>
2017-01-27 12:30:43 +0800

    learn the url parameters.

--- MEAN/api/routes/index.js
+++ MEAN/api/routes/index.js
@@ -5,5 +5,8 @@ var ctrlHotels = require('../controllers/hotels.controller');
 router
     .route('/hotels')
     .get(ctrlHotels.hotelsGetAll);
+router
+    .route("/hotels/:hotelId")
+    .get(ctrlHotels.hotelsGetOne);
 
 module.exports = router;
