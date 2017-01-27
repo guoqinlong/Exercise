22f8bdb66b16ed172566e610fa78436f8de47e38
guoqinlong <guoqinlong@gmail.com>
2017-01-27 12:11:06 +0800

    learn controller.

---
+++ MEAN/api/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+var ctrlHotels = require('../controllers/hotels.controller');
+
+router
+    .route('/hotels')
+    .get(ctrlHotels.hotelsGetAll);
+
+module.exports = router;
