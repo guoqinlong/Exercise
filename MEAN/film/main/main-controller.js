2c2835ee5e944b7dbb350763937b6eaffc04a557
guoqinlong <guoqinlong@gmail.com>
2017-01-30 19:36:43 +0800

    learn customize filters.

--- MEAN/film/main/main-controller.js
+++ MEAN/film/main/main-controller.js
@@ -6,4 +6,8 @@ function MainController(FilmFactory) {
         vm.films = response;
     });
     vm.name = 'Qinlong';
+    vm.date1 = '12 February 2016';
+    vm.date2 = '11 Match 2016';
+    vm.date3 = '03 January 2015';
+    vm.date4 = '25 April 2014';
 }
